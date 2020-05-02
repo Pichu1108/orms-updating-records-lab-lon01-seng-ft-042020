aacfa330778d360b0735f88ec6ddf99fc279e5fc
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-05-02 18:36:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,10 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   attr_accessor :id, :name, :grade
+    def initialize(name,grade,id=nil)
+      @name = name
+      @grade = grade
+    end
 
 
   def save
