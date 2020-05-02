3c41b3c3911e0879a5c6d3984e92264470a318b5
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-05-02 18:41:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
     end
 
     def update
-       sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+       sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
        DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
 
